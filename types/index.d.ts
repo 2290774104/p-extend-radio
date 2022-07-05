66d69ab05c06f6b759d83b0bc52077d1e8759799
唐皓晨 <2290774104@qq.com>
2022-07-05 15:18:39 +0800

    init

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+
+import Vue from 'vue'
+import { ElInput } from 'element-ui/types/input'
+// for future
+// export interface InstallationOptions {}
+// export function install (vue: typeof Vue, options: InstallationOptions): void
+
+// now
+export function install(vue: typeof Vue): void
